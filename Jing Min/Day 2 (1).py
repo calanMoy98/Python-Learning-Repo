5537365370ea518c86a4bb256bf3b9d2844ff630
JamesWoo93 <78687155+JamesWoo93@users.noreply.github.com>
2021-02-19 03:08:51 +0800

    Create Day 2 (1).py

---
+++ Jing Min/Day 2 (1).py
@@ -0,0 +1,18 @@
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+print(friends[-2])
+print(friends[1:])
+
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+friends [1] = "Mike"
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+print(friends[-2])
+print(friends[1:])
